eb36d322d3647de6c98a140fa3a22b0331c96ece
Alayna Johnston <55775759+ayyjayy2@users.noreply.github.com>
2020-08-26 09:14:42 -0400

    Add files via upload

---
+++ List.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+class List {
+
+    public static void main(String[] args) {
+        ArrayList<String> hobbies = new ArrayList<String>(); //creates new arraylist
+
+        //add items to array list
+        hobbies.add("art");
+        hobbies.add("coding");
+        hobbies.add("exploring");
+        hobbies.add("crafting");
+
+        //print items in array list
+        System.out.println(hobbies);
+
+        //accessing size of arraylist
+        System.out.println("Hobbies length: " + hobbies.size());
+
+        //find item by index using get
+        System.out.println(hobbies.get(2));
+
+        //change value of item using set
+        hobbies.set(3, "exercising");
+
+        //print items in array list
+        System.out.println(hobbies);
+
+        //remove item using remove()
+        hobbies.remove(3);
+
+        //retrieve index of item using indexOf()
+        System.out.println(hobbies.indexOf("art"));
+
+        //print items in array list
+        System.out.println(hobbies);
+    }
+
+}
